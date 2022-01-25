a38700d032a1b8e2f6eb01f909193403ddb11067
elizaveta <elizaveta.nurzhanova@gmail.com>
2022-01-26 01:41:07 +0300

    v4

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function Calc(operation, a, b) {
     }
     const isNotValid = (typeof operation === 'undefined' || typeof a !== 'number' || typeof b !== 'number');
     if (isNotValid) {
-        console.log('Error');
-    } else if (operations[operation] !== undefined) {
-    console.log(operations[operation]);
+        return ('Error');
+    }
+    if (operations[operation]) {
+        return(operations[operation]);
     } else {
-        console.log('unknown operation');
+        return('unknown operation');
     }
 }
-Calc('sum', 2, 3);
-Calc('div', 6, 3)
-Calc(2, 3)
-Calc('sum', '2', 3)
-Calc(5, 2, 3);
+console.log(Calc('sum', 2, 3));
+console.log(Calc(1, 2, 3));
+console.log(Calc('sum', 2, '3'));
+console.log(Calc(1, 2));
